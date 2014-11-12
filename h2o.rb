0026126db6e1d2db30b55498f3ed6054a3a4680d
n0ts <naoya.n@gmail.com>
2014-11-13 08:30:03 +0900

    Refatoring h2o Formula

--- h2o.rb
+++ h2o.rb
@@ -1,17 +1,17 @@
-require "formula"
+require 'formula'
 
 class H2o < Formula
-  homepage "https://github.com/h2o/h2o"
-  url "https://github.com/h2o/h2o.git", :branch => "master"
-  head "https://github.com/h2o/h2o.git"
-  version "latest"
+  homepage 'https://github.com/h2o/h2o'
+  url 'https://github.com/h2o/h2o.git', :branch => 'master'
+  head 'https://github.com/h2o/h2o.git'
+  version 'latest'
 
-  depends_on "cmake" => :build
+  depends_on 'cmake' => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "h2o"
+    system 'cmake', '.', *std_cmake_args
+    system 'make', 'h2o'
 
-    bin.install "h2o"
+    bin.install 'h2o'
  end
 end
